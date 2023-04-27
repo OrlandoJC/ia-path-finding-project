2cb6f29b52f16a50dc26611668e12a0b04afc5c3
Orlando de Jesus C <cuximyamaorlandodejesus@gmail.com>
2023-04-26 22:11:05 -0600

    add updates
    add
    +path solutions
    +obstacles
    +add boxes

--- utils/graphics.py
+++ utils/graphics.py
@@ -1,6 +1,15 @@
 def colocarCaja(canvas, x, y, start=0, base=30):
   canvas.create_rectangle((start + x * base, start + y * base), (start + x * base + 60, start + y * base+60), fill='#33A1FF', outline='#33A1FF', tags = ("box","box{x}{y}"))
 
+def boxByReference(canvas, x, y, start=0, base=30):
+  return canvas.create_rectangle((start + x * base, start + y * base), (start + x * base + 60, start + y * base+60), fill='#b3dbff', outline='#b3dbff', tags = ("box","box{x}{y}"))
+
+def colocarObstaculo(canvas, x, y, tam, start=0, base=30):
+  canvas.create_rectangle((start + x * base, start + y * base), (start + x * base + tam*30, start + y * base+30), fill='#77554E', outline='#77554E', tags = ("box", "obstacle","box{x}{y}"))
+
+def obstacleReference(canvas, x, y, tam, start=0, base=30):
+  return canvas.create_rectangle((start + x * base, start + y * base), (start + x * base + tam*30, start + y * base+30), fill='#77554E', outline='#77554E', tags = ("box", "obstacle","box{x}{y}"))
+
 def crearCuadricula(canvas):
   for i in range(30):
     canvas.create_line((0, 30 + (i * 30)), (700, 30 + (i * 30)),width=1, fill="#EBEBFC")  # horizontal
